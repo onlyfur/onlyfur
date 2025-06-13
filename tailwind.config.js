528b34e9028c5c0bf744f69ca20ff401c158ffd7
K3NOXOFFICIAL <kenoschreibt@gmail.com>
2025-06-13 03:29:50 +0200

    V4.7.8

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,7 +30,10 @@ module.exports = {
 				border: 'hsl(var(--border))',
 				input: 'hsl(var(--input))',
 				ring: 'hsl(var(--ring))',
-				background: withOpacity('--background'),
+				background: ({ opacityValue }) =>
+					opacityValue === undefined
+						? 'hsl(var(--background))'
+						: `hsl(var(--background) / ${opacityValue})`,
 				foreground: 'hsl(var(--foreground))',
 				primary: {
 					DEFAULT: '#2B5D3A',
